f936be11a16901a40b63bddd977b4468ffc48428
Fatima Moeed <fatimamoeed322@gmail.com>
2025-05-29 09:36:21 -0400

    added multiply method

--- test1.java
+++ test1.java
@@ -7,4 +7,12 @@ public class test1{
     public static int sum(int x, int y){
         return x + y;
     }
+    
+    public static int multiply(int x, int y){
+        int result = 0;
+        for(int i = 0; i < y; i++){
+            result += x;
+        }
+        return result;
+    }
 }
